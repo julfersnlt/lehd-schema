cc4f95cf2781be7241f7184c7cf2ef8d8cfd2d29
Jody Hoon-Starr <jodyhoonstarr@gmail.com>
2021-11-24 15:36:47 -0500

    Feature: build dynamic index page for gh-pages

---
+++ github/generate_index.sh
@@ -0,0 +1,54 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
+
+############################################################
+# Help                                                     #
+############################################################
+help() {
+  # Display help
+  echo "Generate an index page to be deployed into the ../dist directory"
+  echo
+  echo "Purpose: this script will fill in an environment variable which allows"
+  echo "for the dynamic generation of an index.html page on github. It is not"
+  echo "intended to be run manually but rather as a part of the build process."
+  echo
+  echo "Syntax: generate_index.sh [-r <user/repo string>]"
+  echo "options:"
+  echo "h     Print this Help."
+  echo "r     Generate an index file for this repos gh-pages branch."
+  echo
+  echo "Example:"
+  echo "./generate_index.sh -r username/repositoryname"
+  echo
+}
+
+############################################################
+# Process the input options. Add options as needed.        #
+############################################################
+# No arguments
+if [[ $# -eq 0 ]]; then
+  help
+  exit 1
+fi
+
+# Parse arguments
+while getopts ":hr:" option; do
+  case $option in
+  h) # display help
+    help
+    exit 1
+    ;;
+  r) # Enter a schema version
+    repository=$OPTARG ;;
+  \?) # Invalid option
+    echo "Error: invalid argument"
+    echo
+    help
+    exit 1
+    ;;
+  esac
+done
+
+export REPOSITORY=$repository
+envsubst < ${SCRIPT_DIR}/index.tmpl > ${SCRIPT_DIR}/../dist/index.html
